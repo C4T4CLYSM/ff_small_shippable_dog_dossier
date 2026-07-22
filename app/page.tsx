fd4f0de33152f4be54d5f837b776f8ad66f42d03
C4T4CLYSM <lesliebeteta@protonmail.ch>
2026-07-21 18:02:23 -0700

    Bigger hero and wordmark

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
           <Image
             src="/wordmark.png"
             alt="Dog Dossier"
-            width={300}
-            height={72}
+            width={380}
+            height={90}
             className="object-contain"
             priority
           />
@@ -39,7 +39,7 @@ export default function Home() {
       </header>
 
       {/* Section 1: Hero */}
-      <section className="relative flex min-h-[700px] items-end overflow-hidden pb-16">
+      <section className="relative flex min-h-[85vh] items-end overflow-hidden pb-16">
         <Image
           src="/hero-dog.jpg"
           alt="Dog sitting on a bridge"
